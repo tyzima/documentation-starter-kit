2552ba759edadd2ae5fa2075e8b2f8185ec4ccad
tyzima <ty@zimagear.com>
2024-06-28 00:53:16 -0400

    404

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,26 @@
-// export { locales as middleware } from 'nextra/locales'
-
 import { withLocales } from "nextra/locales";
 import { NextResponse } from "next/server";
-
-// export const config = {
-//   matcher: ['/', '/index'],
-// }
 
 export const middleware = withLocales((request) => {
     const basicAuth = request.headers.get("authorization");
     const url = request.nextUrl;
-    const contentRoute = /\.(jpe?g|svg|png|webmanifest|xml|ico|txt)$/.test(request.nextUrl.pathname);
+    const contentRoute = /\/(jpe?g|svg|png|webmanifest|xml|ico|txt)$/.test(request.nextUrl.pathname);
+
     if (contentRoute) return NextResponse.next();
+
     if (basicAuth) {
         const authValue = basicAuth.split(" ")[1];
         const [user, pwd] = atob(authValue).split(":");
+
         if (user === "LAX" && pwd === "norwalk") {
             return NextResponse.next();
         } else {
             request.headers.delete("authorization");
-            url.pathname = "/api/auth";
+            url.pathname = "/404"; // Redirect to 404 page for incorrect credentials
             return NextResponse.rewrite(url);
         }
     }
-    url.pathname = "/api/auth";
+
+    url.pathname = "/404"; // Redirect to 404 page if the auth is cancelled or missing
     return NextResponse.rewrite(url);
 });

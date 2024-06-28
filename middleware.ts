74e4c6104bde986cead5d888d61739f7d0e7b99b
tyzima <ty@zimagear.com>
2024-06-28 01:05:47 -0400

    404

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,25 @@
-// export { locales as middleware } from 'nextra/locales'
-
 import { withLocales } from "nextra/locales";
 import { NextResponse } from "next/server";
 
-// export const config = {
-//   matcher: ['/', '/index'],
-// }
-
 export const middleware = withLocales((request) => {
-    const basicAuth = request.headers.get("authorization");
-    const url = request.nextUrl;
-    const contentRoute = /\.(jpe?g|svg|png|webmanifest|xml|ico|txt)$/.test(request.nextUrl.pathname);
-    if (contentRoute) return NextResponse.next();
-    if (basicAuth) {
-        const authValue = basicAuth.split(" ")[1];
-        const [user, pwd] = atob(authValue).split(":");
-        if (user === "Lax" && pwd === "norwalk") {
-            return NextResponse.next();
-        } else {
-            request.headers.delete("authorization");
-            url.pathname = "/api/auth";
-            return NextResponse.rewrite(url);
-        }
+  const basicAuth = request.headers.get("authorization");
+  const url = request.nextUrl;
+  const contentRoute = /\.(jpe?g|svg|png|webmanifest|xml|ico|txt)$/.test(request.nextUrl.pathname);
+
+  if (contentRoute) return NextResponse.next();
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+    if (user === "Lax" && pwd === "norwalk") {
+      return NextResponse.next();
+    } else {
+      request.headers.delete("authorization");
+      url.pathname = "/404"; // Changed to 404
+      return NextResponse.rewrite(url);
     }
-    url.pathname = "/api/auth";
-    return NextResponse.rewrite(url);
+  }
+
+  url.pathname = "/404"; // Changed to 404
+  return NextResponse.rewrite(url);
 });
